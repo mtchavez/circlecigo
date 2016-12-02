dd9f41eef392530e91a0061890db193f46d277fc
Chavez <matthew@el-chavez.me>
2016-12-02 12:59:00 -0800

    Project unfollow
    - Add unfollow action
    - Add unfollow tests

--- project_test.go
+++ project_test.go
@@ -52,3 +52,42 @@ func TestClient_Follow(t *testing.T) {
 		t.Errorf("Expected to be following project")
 	}
 }
+
+func TestClient_Unfollow_unauthorized(t *testing.T) {
+	startTestServer()
+	defer stopTestServer()
+	path := fmt.Sprintf("/project/%s/%s/unfollow", testUsername, testReponame)
+	testMux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		checkMethod(t, r, "GET")
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprint(w, `{"message": "You must log in first"}`)
+	})
+	_, apiResp := testClient.Unfollow(testUsername, testReponame)
+	if apiResp.Success() {
+		t.Errorf("Expected response to not be successful without token")
+	}
+	if apiResp.Response.StatusCode != http.StatusUnauthorized {
+		t.Errorf("Expected unauthorized code but got %v", apiResp.Response.StatusCode)
+	}
+}
+
+func TestClient_Unfollow(t *testing.T) {
+	startTestServer()
+	defer stopTestServer()
+	path := fmt.Sprintf("/project/%s/%s/unfollow", testUsername, testReponame)
+	testMux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		checkMethod(t, r, "GET")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, `{"followed": false}`)
+	})
+	follow, apiResp := testClient.Unfollow(testUsername, testReponame)
+	if !apiResp.Success() {
+		t.Errorf("Expected response to be successful")
+	}
+	if apiResp.Response.StatusCode != http.StatusOK {
+		t.Errorf("Expected status OK but got %v", apiResp.Response.StatusCode)
+	}
+	if follow.Followed {
+		t.Errorf("Expected to not be following project")
+	}
+}
