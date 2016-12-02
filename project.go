06345444128b724d0fb27f238f6ca70bf7b73d50
Chavez <matthew@el-chavez.me>
2016-12-02 12:59:00 -0800

    Project unfollow
    - Add unfollow action
    - Add unfollow tests

--- project.go
+++ project.go
@@ -13,3 +13,10 @@ func (client *Client) Follow(username, project string) (*ProjectFollow, *ApiResp
 	apiResp := client.request("GET", path, nil, nil, follow)
 	return follow, apiResp
 }
+
+func (client *Client) Unfollow(username, project string) (*ProjectFollow, *ApiResponse) {
+	follow := &ProjectFollow{}
+	path := fmt.Sprintf("/project/%s/%s/unfollow", username, project)
+	apiResp := client.request("GET", path, nil, nil, follow)
+	return follow, apiResp
+}
