d58be4810c7e10a694178f14926169c01d2ad27f
Chavez <matthew@el-chavez.me>
2016-12-07 17:00:18 -0800

    Project clear cache
    - Add struct and request for clearing project cache
    - Add tests for clearing cache

--- project_test.go
+++ project_test.go
@@ -86,3 +86,43 @@ func TestClient_Unfollow(t *testing.T) {
 		t.Errorf("Expected to not be following project")
 	}
 }
+
+func TestClient_ClearCache_unauthorized(t *testing.T) {
+	startTestServer()
+	defer stopTestServer()
+	path := fmt.Sprintf("/project/%s/%s/build-cache", testUsername, testReponame)
+	testMux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		checkMethod(t, r, "DELETE")
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprint(w, `{"message": "You must log in first"}`)
+	})
+	_, apiResp := testClient.ClearCache(testUsername, testReponame)
+	if apiResp.Success() {
+		t.Errorf("Expected response to not be successful without token")
+	}
+	if apiResp.Response.StatusCode != http.StatusUnauthorized {
+		t.Errorf("Expected unauthorized code but got %v", apiResp.Response.StatusCode)
+	}
+}
+
+func TestClient_ClearCache(t *testing.T) {
+	startTestServer()
+	defer stopTestServer()
+	path := fmt.Sprintf("/project/%s/%s/build-cache", testUsername, testReponame)
+	testMux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		checkMethod(t, r, "DELETE")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, `{"status": "build cache deleted"}`)
+	})
+	clearCache, apiResp := testClient.ClearCache(testUsername, testReponame)
+
+	if !apiResp.Success() {
+		t.Errorf("Expected response to be successful")
+	}
+	if apiResp.Response.StatusCode != http.StatusOK {
+		t.Errorf("Expected status OK but got %v", apiResp.Response.StatusCode)
+	}
+	if clearCache.Status != "build cache deleted" {
+		t.Errorf("Expected status to be %s but got %s", "build cache deleted", clearCache.Status)
+	}
+}
