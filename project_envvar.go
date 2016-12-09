62c1ad5e44de5d3ac4f3478e6d4b1960b2c28d05
Chavez <matthew@el-chavez.me>
2016-12-09 08:31:47 -0800

    Project add envvar
    - Add request for adding envvar
    - Test adding envvar

--- project_envvar.go
+++ project_envvar.go
@@ -1,6 +1,7 @@
 package circleci
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -18,4 +19,15 @@ func (client *Client) EnvVars(username, project string) ([]*EnvVar, *APIResponse
 	path := fmt.Sprintf("/project/%s/%s/envvar", username, project)
 	apiResp := client.request(http.MethodGet, path, nil, nil, &envVars)
 	return envVars, apiResp
+}
+
+// AddEnvVar calls the /project/:username/:project/envvar endpoint
+// to set an ENV variable for the project builds
+func (client *Client) AddEnvVar(username, project, name, value string) (*EnvVar, *APIResponse) {
+	envVar := &EnvVar{}
+	addVar := &EnvVar{Name: name, Value: value}
+	body, _ := json.Marshal(addVar)
+	path := fmt.Sprintf("/project/%s/%s/envvar", username, project)
+	apiResp := client.request(http.MethodPost, path, nil, body, &envVar)
+	return envVar, apiResp
 }
