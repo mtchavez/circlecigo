ab6127990896e95742a0473f03cbe60eb632808e
Chavez <matthew@el-chavez.me>
2016-12-08 22:31:26 -0800

    Add test for parsing of bad JSON

--- circleci_test.go
+++ circleci_test.go
@@ -1,6 +1,7 @@
 package circleci
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -47,5 +48,30 @@ func TestNewClient(t *testing.T) {
 	client := NewClient(TestToken)
 	if client.Token != TestToken {
 		t.Errorf("Expected Token to be set to %s but got %s", TestToken, client.Token)
+	}
+}
+
+func TestClient_EnvVars_badJSON(t *testing.T) {
+	startTestServer()
+	defer stopTestServer()
+	path := fmt.Sprintf("/project/%s/%s/envvar", testUsername, testReponame)
+	testMux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		checkMethod(t, r, http.MethodGet)
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, `[{"name": "FOO", "value": "BAR"}, {"name": "BAZ", "value": "FIZZ"`)
+	})
+	envVars, apiResp := testClient.EnvVars(testUsername, testReponame)
+	if apiResp.Success() {
+		t.Errorf("Expected response to not be successful")
+	}
+	if apiResp.Response.StatusCode != http.StatusOK {
+		t.Errorf("Expected status OK but got %v", apiResp.Response.StatusCode)
+	}
+	expectedError := "unexpected end of JSON input"
+	if apiResp.Error.Error() != expectedError {
+		t.Errorf("Expected error message of %s but got %s", expectedError, apiResp.Error.Error())
+	}
+	if len(envVars) != 0 {
+		t.Errorf("Expected to get %d envvars but got %d", 0, len(envVars))
 	}
 }
