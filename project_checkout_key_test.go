3ffd29474c4905642571f5b0ab84ae3c88b7a911
Chavez <matthew@el-chavez.me>
2016-12-10 14:22:02 -0800

    Get project checkout key
    - Add get checkout key request
    - Add tests for checkout key found and not found

--- project_checkout_key_test.go
+++ project_checkout_key_test.go
@@ -32,3 +32,52 @@ func TestClient_CheckoutKeys(t *testing.T) {
 		t.Errorf("Expected to get type deploy-key but got %s", keys[0].Type)
 	}
 }
+
+func TestClient_GetCheckoutKey_notFound(t *testing.T) {
+	startTestServer()
+	defer stopTestServer()
+	path := fmt.Sprintf("/project/%s/%s/checkout-key/not-real", testUsername, testReponame)
+	testMux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		checkMethod(t, r, http.MethodGet)
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, `{"message": "checkout key not found"}`)
+	})
+	key, apiResp := testClient.GetCheckoutKey(testUsername, testReponame, "not-real")
+	if apiResp.Success() {
+		t.Errorf("Expected response not to be successful")
+	}
+	if apiResp.Response.StatusCode != http.StatusNotFound {
+		t.Errorf("Expected status not found but got %v", apiResp.Response.StatusCode)
+	}
+	if apiResp.ErrorResponse.Message != "checkout key not found" {
+		t.Errorf("Expected checkout key not found but got %v", apiResp.ErrorResponse.Message)
+	}
+	if key.Type == "deploy-key" {
+		t.Errorf("Expected not to get type deploy-key")
+	}
+}
+
+func TestClient_GetCheckoutKey(t *testing.T) {
+	startTestServer()
+	defer stopTestServer()
+	testFingerprint := "c9:0b:1c:4f:d5:65:56:b9:ad:88:f9:81:2b:37:74:2f"
+	path := fmt.Sprintf("/project/%s/%s/checkout-key/%s", testUsername, testReponame, testFingerprint)
+	testMux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		checkMethod(t, r, http.MethodGet)
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, `{"public_key": "ssh-rsa ...", "fingerprint": "%s", "type": "deploy-key"}`, testFingerprint)
+	})
+	key, apiResp := testClient.GetCheckoutKey(testUsername, testReponame, testFingerprint)
+	if !apiResp.Success() {
+		t.Errorf("Expected response to be successful")
+	}
+	if apiResp.Response.StatusCode != http.StatusOK {
+		t.Errorf("Expected status OK but got %v", apiResp.Response.StatusCode)
+	}
+	if key.Type != "deploy-key" {
+		t.Errorf("Expected key type to be deploy-key but got %s", key.Type)
+	}
+	if key.Fingerprint != testFingerprint {
+		t.Errorf("Expected key type to be %s but got %s", testFingerprint, key.Fingerprint)
+	}
+}
